451bea9a9139c88812000d3b42fcf94f8bac9ac1
Gerard Harkema <ga.harkema@avans.nl>
2022-03-25 12:30:42 +0100

    modified installation script, made relative

--- install/install.bash
+++ install/install.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd $HOME/universal_robots_ws/src
+#cd $HOME/universal_robots_ws/src
+cd ../..
 
 git clone -b boost https://github.com/UniversalRobots/Universal_Robots_Client_Library.git
 git clone https://github.com/UniversalRobots/Universal_Robots_ROS_Driver.git
