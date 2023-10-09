7e02fa5721b68c0059b4ea13687ffc8f88bb798f
Student <student@avans.nl>
2023-10-09 05:43:22 -0700

    20231009

---
+++ install/installV2.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#cd $HOME/universal_robots_ws/src
+cd ../..
+
+git clone -b boost https://github.com/UniversalRobots/Universal_Robots_Client_Library.git
+git clone https://github.com/UniversalRobots/Universal_Robots_ROS_Driver.git
+git clone -b calibration_devel https://github.com/fmauch/universal_robot.git
+git clone https://github.com/ros-industrial/ur_msgs.git
+git clone https://github.com/gavanderhoorn/industrial_robot_status_controller.git
+git clone https://github.com/UniversalRobots/Universal_Robots_ROS_scaled_controllers.git
+git clone https://github.com/UniversalRobots/Universal_Robots_ROS_passthrough_controllers.git
+git clone https://github.com/UniversalRobots/Universal_Robots_ROS_cartesian_control_msgs.git
+git clone https://github.com/UniversalRobots/Universal_Robots_ROS_controllers_cartesian.git
+
+sudo apt update -qq
+cd ..
+rosdep update
+rosdep install --from-paths src --ignore-src -y
+
+catkin b
+
